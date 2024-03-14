1b21ab0a2dc71f9948519d3b5cbd00e506644903
maksb2bb <gein@sfedu.ru>
2024-03-14 22:37:03 +0300

    Commit

--- app/src/main/java/com/example/jctemplate2/MainActivity.kt
+++ app/src/main/java/com/example/jctemplate2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jctemplate2
 
 import android.os.Bundle
+import androidx.compose.material3.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +17,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import com.example.jctemplate2.ui.theme.JCTemplate2Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +34,21 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun MyApp(
+    modifier: Modifier = Modifier,
+) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        }else{
+            Greetings()
+        }
+    }
+}
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -41,39 +58,77 @@ fun MyApp(
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
-                Text(text = "Hello ")
+                Text(text = "Hello, ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
-
-@Preview(
-    showBackground = true,
-    widthDp = 320
-)
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the JCTemplate")
+        Button(
+            modifier = Modifier
+                .padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JCTemplate2Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     JCTemplate2Theme {
         MyApp()
+    }
+}
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview(){
+    JCTemplate2Theme {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview(){
+    JCTemplate2Theme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
